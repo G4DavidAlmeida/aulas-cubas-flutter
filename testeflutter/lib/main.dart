7529ea0b56b78b8c936bbdfb21feffc27bf9f408
David Almeida <almeida.davidc@gmail.com>
2021-04-27 17:20:54 -0300

    controle_gastos project created

--- testeflutter/lib/main.dart
+++ testeflutter/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:splashscreen/splashscreen.dart';
-import 'home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,48 +11,22 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Splash Screen Flutter'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return _introScreen();
-  }
-}
-
-Widget _introScreen() {
-  return Stack(
-    children: <Widget>[
-      SplashScreen(
-        seconds: 5,
-        gradientBackground: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          colors: [Color(0xffED213A), Color(0xff93291E)],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("flutter"),
         ),
-        navigateAfterSeconds: HomeScreen(),
-        loaderColor: Colors.transparent,
-      ),
-      Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/logo.png"),
-            fit: BoxFit.none,
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              Text('ola'),
+              Text('ola'),
+              Text('ola'),
+            ],
           ),
         ),
       ),
-    ],
-  );
+    );
+  }
 }
